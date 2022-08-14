cce0169795095fa3b18382c813a272b6ccfa7e08
ahsnl7 <ahsnl.mi@gmail.com>
2022-08-14 13:29:11 +0200

    stage: change to remote server api

--- src/components/pages/home/component/HomeStatsComponents.js
+++ src/components/pages/home/component/HomeStatsComponents.js
@@ -9,7 +9,7 @@ export default function HomeStatsComponents(props) {
     })
 
     React.useEffect(function() {
-        fetch(`http://127.0.0.1:5000/api/v1/stats/${props.id}`)
+        fetch(`https://dash-formula-one-api.herokuapp.com/api/v1/stats/${props.id}`)
             .then(res => res.json())
             .then(data => setStatsProps(data))
     }, [props.id])
@@ -54,72 +54,3 @@ function GenerateTable({headers, data}) {
     )
 }
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-
-function HomeStatsDriver({drivers}) {
-    const tableData = drivers.map( driver => {
-        const {row_number, driver_fullname, constructors_name, points} = driver
-        return (
-            <tr>
-                <td>{row_number}</td>
-                <td>{driver_fullname}</td>
-                <td>{constructors_name}</td>
-                <td>{points}</td>
-            </tr>
-        )
-    })    
-        
-    return (
-        <div className='home-stats__stats'>
-            <div className='stats__charts'>
-                <p>.score-stats-profile</p>
-            </div>
-            <div className='stats__table'>
-                <table className='table'>
-                    <tr>
-                        <th>Position</th>
-                        <th>Constructor</th>
-                        <th>Team</th>
-                        <th>Points</th>
-                    </tr>
-                    {tableData}
-                </table>
-            </div>
-        </div>
-    )
-
-}
-
-function HomeStatsConstructor({constructors}) {
-    const tableData = constructors.map( constructor => {
-        const {row_number, constructors_name, points} = constructor
-        return (
-            <tr>
-                <td>{row_number}</td>
-                <td>{constructors_name}</td>
-                <td>{points}</td>
-            </tr>
-        )
-    })    
-        
-    return (
-        <div className='home-stats__stats'>
-            <div className='stats__charts'>
-                <p>.score-stats-profile</p>
-            </div>
-            <div className='stats__table'>
-                <table border={1} cellPadding={2} className='table'>
-                    <tr>
-                        <th>Position</th>
-                        <th>Constructor</th>
-                        <th>Points</th>
-                    </tr>
-                    {tableData}
-                </table>
-            </div>
-        </div>
-    )
-}
